24fe1b51f5eab543dc60a19e0b60c2e4984d2261
hkastler <hkastler+github@gmail.com>
2018-03-29 13:53:38 -0500

    scheduler

---
+++ src/main/java/com/hkstlr/app/control/BlogMessageFetchScheduler.java
@@ -0,0 +1,41 @@
+package com.hkstlr.app.control;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+
+import com.hkstlr.app.boundary.Index;
+
+/**
+ *
+ * @author henry.kastler
+ */
+@Singleton
+public class BlogMessageFetchScheduler {
+
+	@Inject
+	Index index;
+
+	@Inject
+	Event<String> event;
+
+	Logger log = Logger.getLogger(BlogMessageFetchScheduler.class.getName());
+
+	@Schedule(second = "0", minute = "0", hour = "*/1", persistent = false)
+	public void fetchMessages() {
+		try {
+			log.log(Level.INFO, "getting blog msgs");
+			//System.out.println("scheduler");
+			index.fetchAndSetBlogMessages();
+			log.log(Level.INFO, "blog msgs retrieved");
+			event.fire("blog msgs");
+		} catch (Exception ex) {
+			log.log(Level.SEVERE, "error", ex);
+		}
+	}
+
+}
